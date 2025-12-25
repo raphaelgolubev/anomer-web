07d12db076b7d01535dd9af64defef17acb5d0b7
Raphael Golubev <raphael.golubev@gmail.com>
2025-12-25 14:53:50 +0300

    feat: creater container for Functional Keys

---
+++ src/lib/components/FuncKeysContainer.svelte
@@ -0,0 +1,20 @@
+<script>
+	// Принимаем children как специальный пропс-сниппет
+	let { children } = $props();
+</script>
+
+<div class="function-keys">
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
+
+<style>
+	.function-keys {
+		display: flex;
+		gap: 5px;
+		border-top: 1px solid #90fbab;
+		padding-top: 10px;
+		flex-wrap: wrap;
+	}
+</style>
