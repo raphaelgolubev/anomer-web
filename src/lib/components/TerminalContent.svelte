06f6eb9b0b66b812c392e9b2bfc9d09ba2b71e12
Raphael Golubev <raphael.golubev@gmail.com>
2025-12-25 14:54:39 +0300

    feat: created terminal's content container

---
+++ src/lib/components/TerminalContent.svelte
@@ -0,0 +1,22 @@
+<script>
+	let { children } = $props();
+</script>
+
+<div class="terminal-content">
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
+
+<style>
+	.terminal-content {
+		display: flex;
+		flex-direction: row;
+		position: relative;
+		width: 100%;
+		height: 100%;
+		margin-bottom: 1rem;
+		overflow-y: auto;
+		overflow-x: hidden;
+	}
+</style>
