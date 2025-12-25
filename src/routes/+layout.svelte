5ce8f5d00f959fc4e486f387f82ca7c32d82c310
Raphael Golubev <raphael.golubev@gmail.com>
2025-12-25 14:57:04 +0300

    feat: created main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,23 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+	import CrtOverlay from '$lib/components/CRTOverlay.svelte';
+	import Scanlines from '$lib/components/Scanlines.svelte';
+	import Terminal from '$lib/components/Terminal.svelte';
+	import TerminalHeader from '$lib/components/TerminalHeader.svelte';
+
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+
+
+<Terminal>
+	<Scanlines />
+	<TerminalHeader />
+
+	{@render children()}
+</Terminal>
+
+<CrtOverlay />
