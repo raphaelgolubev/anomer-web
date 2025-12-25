f7ffce98d6e9ad021a4f0c54f7167fcf03e54390
Raphael Golubev <raphael.golubev@gmail.com>
2025-12-25 14:53:17 +0300

    feat: created top layer with animated scanlines effect

---
+++ src/lib/components/Scanlines.svelte
@@ -0,0 +1,30 @@
+<div class="scanlines"></div>
+
+<style>
+	.scanlines {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: repeating-linear-gradient(
+			0deg,
+			transparent 0px,
+			transparent 2px,
+			rgba(86, 252, 150, 0.3) 2px,
+			rgba(66, 241, 86, 0.3) 3px
+		);
+		pointer-events: none;
+		z-index: 3;
+		animation: scanline-move 25s linear infinite;
+	}
+
+	@keyframes scanline-move {
+		0% {
+			transform: translateY(0);
+		}
+		100% {
+			transform: translateY(-3px);
+		}
+	}
+</style>
