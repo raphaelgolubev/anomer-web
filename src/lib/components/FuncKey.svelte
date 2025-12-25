1f0df2a99ed75f995ed3e9d179a0537e709c0eb4
Raphael Golubev <raphael.golubev@gmail.com>
2025-12-25 14:53:35 +0300

    feat: added Functional Key component

---
+++ src/lib/components/FuncKey.svelte
@@ -0,0 +1,33 @@
+<script>
+	// Принимаем children как специальный пропс-сниппет
+	let { children, link } = $props();
+</script>
+
+<a href={link} class="function-key">
+	{#if children}
+		{@render children()}
+	{/if}
+</a>
+
+<style>
+	.function-key {
+		flex: 1;
+		background: #00441e;
+		padding: 5px 8px;
+		text-align: center;
+		border: 1px solid #61f16d;
+		font-size: 12px;
+		min-width: 80px;
+		cursor: pointer;
+		transition: all 0.2s;
+	}
+
+	.function-key:hover {
+		background: #007708;
+		box-shadow: 0 0 10px rgba(0, 255, 68, 0.5);
+	}
+
+	.function-key:active {
+		background: #00991f;
+	}
+</style>
