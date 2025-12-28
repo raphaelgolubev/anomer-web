412438144cc25fdb73e0570c5a3343fbe9136955
Raphael Golubev <raphael.golubev@gmail.com>
2025-12-28 14:53:51 +0300

    refactor: unfold short 'background' syntax

--- src/lib/components/Scanlines.svelte
+++ src/lib/components/Scanlines.svelte
@@ -7,13 +7,31 @@
 		left: 0;
 		width: 100%;
 		height: 100%;
-		background: repeating-linear-gradient(
+
+		background-color: rgba(0, 0, 0, 0);
+		background-position-x: 0%;
+		background-position-y: 0%;
+		background-repeat: repeat;
+		background-attachment: scroll;
+		background-size: auto;
+		background-origin: padding-box;
+		background-clip: border-box;
+
+		background-image: repeating-linear-gradient(
 			0deg,
 			transparent 0px,
 			transparent 2px,
 			rgba(86, 252, 150, 0.3) 2px,
 			rgba(66, 241, 86, 0.3) 3px
 		);
+
+		/* background: repeating-linear-gradient(
+			0deg,
+			transparent 0px,
+			transparent 2px,
+			rgba(86, 252, 150, 0.3) 2px,
+			rgba(66, 241, 86, 0.3) 3px
+		); */
 		pointer-events: none;
 		z-index: 3;
 		animation: scanline-move 25s linear infinite;
