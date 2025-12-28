0caa66c0e37eb63ca8d0b27fa6f484d0bfea6097
Raphael Golubev <raphael.golubev@gmail.com>
2025-12-28 14:56:26 +0300

    feat: improved root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,25 +3,47 @@
 
 	import TerminalContent from '$lib/components/TerminalContent.svelte';
 	import FuncKey from '$lib/components/FuncKey.svelte';
-	import FuncKeyContainer from '$lib/components/FuncKeysContainer.svelte';
+	import TerminalFooter from '$lib/components/TerminalFooter.svelte';
 	import TerminalInput from '$lib/components/TerminalInput.svelte';
 	import TypewriterLine from '$lib/components/TypeWriter.svelte';
 
 	import { initSystemInfo, systemInfo } from '$lib/fingerprint.svelte';
 
+	let printable = $state<string[]>([]);
 	let visibleLines = $state<string[]>([]);
 	let currentLineIndex = $state(0);
 
+	let userInputContainer: HTMLDivElement;
+
 	function startNextLine() {
-		const lines = systemInfo.asLines;
-		if (currentLineIndex < lines.length) {
-			visibleLines = [...visibleLines, lines[currentLineIndex]];
+		if (currentLineIndex < printable.length) {
+			visibleLines = [...visibleLines, printable[currentLineIndex]];
+			
 			currentLineIndex++;
 		}
 	}
 
+	function userInput(value: string) {
+		console.log(`userInput is ${value}`);
+
+		let text = document.createElement('p');
+		text.textContent = value;
+
+		userInputContainer.appendChild(text);
+	}
+
 	onMount(async () => {
 		await initSystemInfo();
+
+		printable = [...systemInfo.asLines, "====="];
+		
+		const welcome: string[] = [
+			"Добро пожаловать в систему.",
+			"Введите свое имя:"
+		];
+
+		printable.push(...welcome);
+
 		startNextLine();
 	});
 </script>
@@ -29,21 +51,24 @@
 <TerminalContent>
 	<div class="terminal-output">
 		{#each visibleLines as line, i}
-			<TypewriterLine text={line} onComplete={startNextLine} speed={20} />
+			<TypewriterLine text={line} onComplete={startNextLine} speed={5} />
 		{/each}
+		<div class="user-input" bind:this={userInputContainer}>
+			<!-- ... -->
+		</div>
 	</div>
 </TerminalContent>
 
-<TerminalInput prefix="guest@system:~$" />
+<TerminalInput prefix="guest@system:~$" onEnter={userInput}/>
 
-<FuncKeyContainer>
+<TerminalFooter>
 	<FuncKey link="/help">F1 [/help]</FuncKey>
 	<FuncKey link="/auth">F2 [/auth]</FuncKey>
 	<FuncKey link="/register">F3 [/register]</FuncKey>
 	<FuncKey link="/hack">F4 [hack pentagon]</FuncKey>
 	<FuncKey link="/info">F5 [info]</FuncKey>
 	<FuncKey link="/about">F6 [about]</FuncKey>
-</FuncKeyContainer>
+</TerminalFooter>
 
 <style>
 	.terminal-output {
