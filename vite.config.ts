c8a71cf7281989c07063fe10a6100ad0520b9e4a
Raphael Golubev <raphael.golubev@gmail.com>
2026-03-07 15:27:15 +0300

    build: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,24 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [sveltekit(), devtoolsJson()],
 
-	test: {
-		expect: { requireAssertions: true },
-
-		projects: [
-			{
-				extends: './vite.config.ts',
-
-				test: {
-					name: 'client',
-
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }]
-					},
-
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**']
-				}
-			},
-
-			{
-				extends: './vite.config.ts',
-
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		strictPort: true,
+		hmr: {
+			// Порт для HMR, который мы пробросили в Docker
+			clientPort: 24678 
+		},
+		proxy: {
+			'/api': {
+				target: 'http://backend:8000', // 'backend' — имя сервиса в docker-compose
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
 			}
-		]
-	}
+		}
+	},
 });
