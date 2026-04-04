93b20befd0d36f0dbe07c0cf5fee61f4a0df0f97
Raphael Golubev <raphael.golubev@gmail.com>
2026-04-04 19:11:09 +0300

    last commit in this project

--- desktop/src/bun/index.ts
+++
@@ -1,6 +0,0 @@
-import { BrowserWindow } from "electrobun/bun";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const win = new BrowserWindow({
-  title: "Hello Electrobun",
-  url: "http://localhost:3000",
-});
